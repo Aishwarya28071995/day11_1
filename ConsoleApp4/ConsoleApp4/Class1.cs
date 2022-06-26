fde985fc560c2f8069995f982e5cfd559861631e
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-26 10:11:36 +0530

    Commiting Uc2 files

---
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class InventoryManager
+    {
+        public void getObject()
+        {
+            List<InventoryFactory> list = new List<InventoryFactory>()
+            {
+                new InventoryFactory("rice", 2, 200),
+               new InventoryFactory("wheat", 2, 150),
+             };
+            foreach (InventoryFactory item in list)
+            {
+                Console.WriteLine(item.name);
+            }
+
+            var deserializeobject = "[{ \"name\":\"rice\",\"weight\":2,\"price\":200},{ \"name\":\"wheat\",\"weight\":2,\"price\":150}]";
+
+            //var value = JsonConvert.SerializeObject(list);
+
+            List<InventoryFactory> inventories = JsonConvert.DeserializeObject<List<InventoryFactory>>(deserializeobject);
+
+            InventoryFactory.CalculateValue(inventories);
+        }
+    }
+}
--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -8,54 +8,13 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            List<Inventory> list = new List<Inventory>()
-            {
-                new Inventory("rice", 2, 200),
-               new Inventory("wheat", 2, 150),
-             };
-            foreach(Inventory item in list)
-            {
-                Console.WriteLine(item.name);
-            }
+            InventoryManager manager = new InventoryManager();
+            manager.getObject();
 
-            var deserializeobject = "[{ \"name\":\"rice\",\"weight\":2,\"price\":200},{ \"name\":\"wheat\",\"weight\":2,\"price\":150}]";
 
-            //var value = JsonConvert.SerializeObject(list);
-
-            List<Inventory> inventories = JsonConvert.DeserializeObject<List<Inventory>>(deserializeobject);
-
-            Inventory.CalculateValue(inventories);
-            
 
         }
 
-        public class Inventory
-        {
-            public String name;
-            public int weight;
-            public int price;
-
-            
-            public Inventory(String name,int weight,int price)
-            {
-                this.name = name;
-                this.weight = weight;
-                this.price = price;
-               // Console.WriteLine(name+weight+price);
-            }
-
-            public static void CalculateValue(List<Inventory> list)
-            {
-                foreach(Inventory item in list)
-                {
-                    float weight = item.weight;
-                    float price = item.price;
-                    float value = weight * price;
-                    Console.WriteLine("Total value of item is "+value);
-                }
-
-
-            }
-        }
+        
     }
 }
